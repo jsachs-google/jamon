ccf95b561e365e20fc156e925757d24ba9b225b1
iroberts <iroberts@6afd1a24-8710-0410-9bc4-c69fca332c4c>
2006-01-15 22:47:38 +0000

    Add a switchToProxy action
    git-svn-id: svn://svn.code.sf.net/p/jamon/code/trunk@1805 6afd1a24-8710-0410-9bc4-c69fca332c4c

--- eclipse/uiPlugin/src/org/jamon/eclipse/popup/SwitchToProxyAction.java
+++ eclipse/uiPlugin/src/org/jamon/eclipse/popup/SwitchToProxyAction.java
@@ -3,11 +3,12 @@ package org.jamon.eclipse.popup;
 import org.eclipse.core.resources.IFile;
 import org.jamon.eclipse.TemplateResources;
 
-public class SwitchToImplAction extends AbstractSwitchToGeneratedFileAction
+public class SwitchToProxyAction extends AbstractSwitchToGeneratedFileAction
 {
     @Override
     protected IFile getGeneratedFile(TemplateResources p_templateResources)
     {
-        return p_templateResources.getImpl();
+        return p_templateResources.getProxy();
     }
+
 }
