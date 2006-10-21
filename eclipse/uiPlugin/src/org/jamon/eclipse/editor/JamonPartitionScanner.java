0729fa5ac2c04b868e393f64287808ceec34deeb
iroberts <iroberts@6afd1a24-8710-0410-9bc4-c69fca332c4c>
2006-10-21 22:50:12 +0000

    kill unused private (cuts down on military payroll expenses).
    git-svn-id: svn://svn.code.sf.net/p/jamon/code/trunk@1974 6afd1a24-8710-0410-9bc4-c69fca332c4c

--- eclipse/uiPlugin/src/org/jamon/eclipse/editor/JamonPartitionScanner.java
+++ eclipse/uiPlugin/src/org/jamon/eclipse/editor/JamonPartitionScanner.java
@@ -58,369 +58,367 @@ DEFAULT: everywhere else
 
  */
 
-public class JamonPartitionScanner implements IPartitionTokenScanner 
+public class JamonPartitionScanner implements IPartitionTokenScanner
 {
 
-	public static final String JAMON = IDocument.DEFAULT_CONTENT_TYPE;
-	public static final String ARGS = "__jamon_partition_args";
-	public static final String JAVA = "__jamon_partition_java";
-	public static final String EMIT = "__jamon_partition_emit";
-	public static final String CLASS = "__jamon_partition_class";
-	
-	private static final IToken JAVA_TOKEN = new Token(JAVA);
-	private static final IToken EMIT_TOKEN = new Token(EMIT);
-	private static final IToken JAMON_TOKEN = new Token(JAMON);
-	private static final IToken ARGS_TOKEN = new Token(ARGS);
-	private static final IToken CLASS_TOKEN = new Token(CLASS);
-	
-	public static final String[] JAMON_PARTITION_TYPES = new String[] {
-		JAMON_TOKEN.getData().toString(),
-		ARGS_TOKEN.getData().toString(),
-		JAVA_TOKEN.getData().toString(),
-		EMIT_TOKEN.getData().toString(),
-		CLASS_TOKEN.getData().toString(),
-	};
-	
-	private IDocument document;
-	private int offset;
-	private int tokenOffset;
-	private int tokenLength;
-	private int length;
-	private IToken currentContent;
-	private int partitionOffset;
-	private int limit;
-	
-	private static IToken tokenFor(String contentType) 
-	{
-		if (contentType == null) 
-		{
-			return JAMON_TOKEN;
-		}
-		else if (ARGS_TOKEN.getData().toString().equals(contentType)) 
-		{
-			return ARGS_TOKEN;
-		}
-		else if (JAVA_TOKEN.getData().toString().equals(contentType)) 
-		{
-			return JAVA_TOKEN;
-		}
-		else if (EMIT_TOKEN.getData().toString().equals(contentType)) 
-		{
-			return EMIT_TOKEN;
-		}
-		else if (CLASS_TOKEN.getData().toString().equals(contentType)) 
-		{
-			return CLASS_TOKEN;
-		}
-		else if (JAMON_TOKEN.getData().toString().equals(contentType))
-		{
-			return JAMON_TOKEN;
-		}
-		else 
-		{
-			throw new IllegalArgumentException("unknown content type " + contentType);
-		}
-	}
-	
-	private int endOffset;
-	
-	public void setPartialRange(IDocument document, int offset, int length, String contentType, int partitionOffset) 
-	{
-		this.document = document;
-		this.offset = offset;
-		this.length = length;
-		this.endOffset = offset + length;
-		this.currentContent = tokenFor(contentType);
-		this.partitionOffset = partitionOffset;
-		this.limit = offset + length;
-	}
+  public static final String JAMON = IDocument.DEFAULT_CONTENT_TYPE;
+  public static final String ARGS = "__jamon_partition_args";
+  public static final String JAVA = "__jamon_partition_java";
+  public static final String EMIT = "__jamon_partition_emit";
+  public static final String CLASS = "__jamon_partition_class";
 
-	public int getTokenLength() 
-	{
-		return tokenLength;
-	}
-	
-	public int getTokenOffset() 
-	{
-		return tokenOffset;
-	}
-	
-	private int nextChar(int i) 
-	{
-		try 
-		{
-			return document.getChar(i);
-		}
-		catch (BadLocationException e) 
-		{
-			return ICharacterScanner.EOF;
-		}
-	}
-	
-	private boolean lookingAt(int off, String match) 
-	{
-		for (int i = 0; i < match.length(); ++i) 
-		{
-			if (nextChar(i + off) != match.charAt(i)) 
-			{
-				return false;
-			}
-		}
-		return true;
-	}
-	
-	private IToken processDefault() 
-	{
-		int i = offset;
-		while (i < limit) 
-		{
-			if (lookingAt(i, "<%args>")) 
-			{
-				int end = processArgs(i);
-				if (end < 0) 
-				{
-					// no </%args>
-					tokenLength = length - (i - offset);
-					offset = limit;
-				}
-				else 
-				{
-					// found </%args>
-					tokenLength = end - i + 1;
-					offset = end;
-				}
-				tokenOffset = i;
-				return ARGS_TOKEN;
-			}
-			if (lookingAt(i, "<% ")) 
-			{
-				int end = processEmit(i);
-				if (end < 0) 
-				{
-					// no </%args>
-					tokenLength = length - (i - offset);
-					offset = limit;
-				}
-				else 
-				{
-					// found </%args>
-					tokenLength = end - i + 1;
-					offset = end;
-				}
-				tokenOffset = i;
-				return EMIT_TOKEN;
-			}
-			else if (lookingAt(i, "<%java>")) 
-			{
-				int end = processJava(i);
-				if (end < 0) 
-				{
-					// no </%java>
-					tokenLength = length - (i - offset);
-					offset = limit;
-				}
-				else 
-				{
-					// found </%java>
-					tokenLength = end - i + 1;
-					offset = end;
-				}
-				tokenOffset = i;
-				return JAVA_TOKEN;
-			}
-			else if (lookingAt(i, "<%class>")) 
-			{
-				int end = processClass(i);
-				if (end < 0) 
-				{
-					// no </%java>
-					tokenLength = length - (i - offset);
-					offset = limit;
-				}
-				else 
-				{
-					// found </%java>
-					tokenLength = end - i + 1;
-					offset = end;
-				}
-				tokenOffset = i;
-				return CLASS_TOKEN;
-			}
-			/*
-			else if (lookingAt(i, "<%java>")) {
-				offset = i;
-				return JAVA_TOKEN;
-			}
-			*/
-			i++;
-		}
-		tokenLength = i - offset + 1;
-		tokenOffset = offset;
-		offset = i;
-		return JAMON_TOKEN;
-	}
-	
-	public IToken nextToken() 
-	{
-		if (offset >= limit) 
-		{
-			return Token.EOF;
-		}
-		else if (currentContent == JAMON_TOKEN) 
-		{
-			return processDefault();
-		}
-		else 
-		{
-			return xnextToken();
-			/*
-			tokenOffset = 0;
-			tokenLength = length;
-			offset += length;
-			return JAMON_TOKEN;
-			*/
-		}
-	}
-	
-	public IToken xnextToken() 
-	{
-		if (currentContent == ARGS_TOKEN) {
-			int end = processArgs(offset);
-			if (end < 0) 
-			{
-				// didn't see </%args>
-				return currentContent;
-			}
-			else 
-			{
-				// found it
-				tokenLength = end - offset + 1;
-				tokenOffset = end;
-				return JAMON_TOKEN;
-			}
-		}
-		else if (currentContent == JAVA_TOKEN) {
-			int end = processJava(offset);
-			if (end < 0) 
-			{
-				// didn't see </%java>
-				return currentContent;
-			}
-			else 
-			{
-				// found it
-				tokenLength = end - offset + 1;
-				tokenOffset = end;
-				return JAMON_TOKEN;
-			}
-		}
-		else if (currentContent == CLASS_TOKEN) {
-			int end = processClass(offset);
-			if (end < 0) 
-			{
-				// didn't see </%java>
-				return currentContent;
-			}
-			else 
-			{
-				// found it
-				tokenLength = end - offset + 1;
-				tokenOffset = end;
-				return JAMON_TOKEN;
-			}
-		}
-		else if (currentContent == EMIT_TOKEN) {
-			int end = processEmit(offset);
-			if (end < 0) 
-			{
-				// didn't see </%java>
-				return currentContent;
-			}
-			else 
-			{
-				// found it
-				tokenLength = end - offset + 1;
-				tokenOffset = end;
-				return EMIT_TOKEN;
-			}
-		}
-		/*
-		else if (currentContent == JAVA_TOKEN) {
-			return processJava();
-		}
-		*/
-		else
-		{
-			throw new IllegalStateException();
-		}
-	}
-	
-	private int processArgs(int start)
-	{
-		return processSection(start, "</%args>");
-	}
-	
-	private int processJava(int start)
-	{
-		return processSection(start, "</%java>");
-	}
-	
-	private int processClass(int start)
-	{
-		return processSection(start, "</%class>");
-	}
-	
-	private int processEmit(int start)
-	{
-		return processSection(start, "%>");
-	}
-	
-	private int processSection(int start, String endToken)
-	{
-		int i = start;
-		boolean inString = false; // FIXME: need to figure this out if in mid-partition!
-		int lastChar = ICharacterScanner.EOF;
-		while (i < endOffset) 
-		{
-			int c = nextChar(i);
-			if (c == '"') 
-			{
-				if (inString)
-				{
-					if (lastChar != '\\') 
-					{
-						inString = false;
-					}
-				}
-				else 
-				{
-					inString = true;
-				}
-			}
-			else if (! inString) 
-			{
-				if (lookingAt(i, endToken)) 
-				{
-					return i + endToken.length();
-				}
-			}
-			else
-			{
-				if (lastChar == '\\' && c == '\\') 
-				{
-					lastChar = ICharacterScanner.EOF;
-				}
-			}
-			i++;
-			lastChar = c;
-		}
-		return -1;
-	}
+  private static final IToken JAVA_TOKEN = new Token(JAVA);
+  private static final IToken EMIT_TOKEN = new Token(EMIT);
+  private static final IToken JAMON_TOKEN = new Token(JAMON);
+  private static final IToken ARGS_TOKEN = new Token(ARGS);
+  private static final IToken CLASS_TOKEN = new Token(CLASS);
 
-	public void setRange(IDocument document, int offset, int length) 
-	{
-		setPartialRange(document, offset, length, null, -1);
-	}
+  public static final String[] JAMON_PARTITION_TYPES = new String[] {
+    JAMON_TOKEN.getData().toString(),
+    ARGS_TOKEN.getData().toString(),
+    JAVA_TOKEN.getData().toString(),
+    EMIT_TOKEN.getData().toString(),
+    CLASS_TOKEN.getData().toString(),
+  };
+
+  private IDocument document;
+  private int offset;
+  private int tokenOffset;
+  private int tokenLength;
+  private int length;
+  private IToken currentContent;
+  private int limit;
+
+  private static IToken tokenFor(String contentType)
+  {
+    if (contentType == null)
+    {
+      return JAMON_TOKEN;
+    }
+    else if (ARGS_TOKEN.getData().toString().equals(contentType))
+    {
+      return ARGS_TOKEN;
+    }
+    else if (JAVA_TOKEN.getData().toString().equals(contentType))
+    {
+      return JAVA_TOKEN;
+    }
+    else if (EMIT_TOKEN.getData().toString().equals(contentType))
+    {
+      return EMIT_TOKEN;
+    }
+    else if (CLASS_TOKEN.getData().toString().equals(contentType))
+    {
+      return CLASS_TOKEN;
+    }
+    else if (JAMON_TOKEN.getData().toString().equals(contentType))
+    {
+      return JAMON_TOKEN;
+    }
+    else
+    {
+      throw new IllegalArgumentException("unknown content type " + contentType);
+    }
+  }
+
+  private int endOffset;
+
+  public void setPartialRange(IDocument document, int offset, int length, String contentType, int partitionOffset)
+  {
+    this.document = document;
+    this.offset = offset;
+    this.length = length;
+    this.endOffset = offset + length;
+    this.currentContent = tokenFor(contentType);
+    this.limit = offset + length;
+  }
+
+  public int getTokenLength()
+  {
+    return tokenLength;
+  }
+
+  public int getTokenOffset()
+  {
+    return tokenOffset;
+  }
+
+  private int nextChar(int i)
+  {
+    try
+    {
+      return document.getChar(i);
+    }
+    catch (BadLocationException e)
+    {
+      return ICharacterScanner.EOF;
+    }
+  }
+
+  private boolean lookingAt(int off, String match)
+  {
+    for (int i = 0; i < match.length(); ++i)
+    {
+      if (nextChar(i + off) != match.charAt(i))
+      {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private IToken processDefault()
+  {
+    int i = offset;
+    while (i < limit)
+    {
+      if (lookingAt(i, "<%args>"))
+      {
+        int end = processArgs(i);
+        if (end < 0)
+        {
+          // no </%args>
+          tokenLength = length - (i - offset);
+          offset = limit;
+        }
+        else
+        {
+          // found </%args>
+          tokenLength = end - i + 1;
+          offset = end;
+        }
+        tokenOffset = i;
+        return ARGS_TOKEN;
+      }
+      if (lookingAt(i, "<% "))
+      {
+        int end = processEmit(i);
+        if (end < 0)
+        {
+          // no </%args>
+          tokenLength = length - (i - offset);
+          offset = limit;
+        }
+        else
+        {
+          // found </%args>
+          tokenLength = end - i + 1;
+          offset = end;
+        }
+        tokenOffset = i;
+        return EMIT_TOKEN;
+      }
+      else if (lookingAt(i, "<%java>"))
+      {
+        int end = processJava(i);
+        if (end < 0)
+        {
+          // no </%java>
+          tokenLength = length - (i - offset);
+          offset = limit;
+        }
+        else
+        {
+          // found </%java>
+          tokenLength = end - i + 1;
+          offset = end;
+        }
+        tokenOffset = i;
+        return JAVA_TOKEN;
+      }
+      else if (lookingAt(i, "<%class>"))
+      {
+        int end = processClass(i);
+        if (end < 0)
+        {
+          // no </%java>
+          tokenLength = length - (i - offset);
+          offset = limit;
+        }
+        else
+        {
+          // found </%java>
+          tokenLength = end - i + 1;
+          offset = end;
+        }
+        tokenOffset = i;
+        return CLASS_TOKEN;
+      }
+      /*
+      else if (lookingAt(i, "<%java>")) {
+        offset = i;
+        return JAVA_TOKEN;
+      }
+      */
+      i++;
+    }
+    tokenLength = i - offset + 1;
+    tokenOffset = offset;
+    offset = i;
+    return JAMON_TOKEN;
+  }
+
+  public IToken nextToken()
+  {
+    if (offset >= limit)
+    {
+      return Token.EOF;
+    }
+    else if (currentContent == JAMON_TOKEN)
+    {
+      return processDefault();
+    }
+    else
+    {
+      return xnextToken();
+      /*
+      tokenOffset = 0;
+      tokenLength = length;
+      offset += length;
+      return JAMON_TOKEN;
+      */
+    }
+  }
+
+  public IToken xnextToken()
+  {
+    if (currentContent == ARGS_TOKEN) {
+      int end = processArgs(offset);
+      if (end < 0)
+      {
+        // didn't see </%args>
+        return currentContent;
+      }
+      else
+      {
+        // found it
+        tokenLength = end - offset + 1;
+        tokenOffset = end;
+        return JAMON_TOKEN;
+      }
+    }
+    else if (currentContent == JAVA_TOKEN) {
+      int end = processJava(offset);
+      if (end < 0)
+      {
+        // didn't see </%java>
+        return currentContent;
+      }
+      else
+      {
+        // found it
+        tokenLength = end - offset + 1;
+        tokenOffset = end;
+        return JAMON_TOKEN;
+      }
+    }
+    else if (currentContent == CLASS_TOKEN) {
+      int end = processClass(offset);
+      if (end < 0)
+      {
+        // didn't see </%java>
+        return currentContent;
+      }
+      else
+      {
+        // found it
+        tokenLength = end - offset + 1;
+        tokenOffset = end;
+        return JAMON_TOKEN;
+      }
+    }
+    else if (currentContent == EMIT_TOKEN) {
+      int end = processEmit(offset);
+      if (end < 0)
+      {
+        // didn't see </%java>
+        return currentContent;
+      }
+      else
+      {
+        // found it
+        tokenLength = end - offset + 1;
+        tokenOffset = end;
+        return EMIT_TOKEN;
+      }
+    }
+    /*
+    else if (currentContent == JAVA_TOKEN) {
+      return processJava();
+    }
+    */
+    else
+    {
+      throw new IllegalStateException();
+    }
+  }
+
+  private int processArgs(int start)
+  {
+    return processSection(start, "</%args>");
+  }
+
+  private int processJava(int start)
+  {
+    return processSection(start, "</%java>");
+  }
+
+  private int processClass(int start)
+  {
+    return processSection(start, "</%class>");
+  }
+
+  private int processEmit(int start)
+  {
+    return processSection(start, "%>");
+  }
+
+  private int processSection(int start, String endToken)
+  {
+    int i = start;
+    boolean inString = false; // FIXME: need to figure this out if in mid-partition!
+    int lastChar = ICharacterScanner.EOF;
+    while (i < endOffset)
+    {
+      int c = nextChar(i);
+      if (c == '"')
+      {
+        if (inString)
+        {
+          if (lastChar != '\\')
+          {
+            inString = false;
+          }
+        }
+        else
+        {
+          inString = true;
+        }
+      }
+      else if (! inString)
+      {
+        if (lookingAt(i, endToken))
+        {
+          return i + endToken.length();
+        }
+      }
+      else
+      {
+        if (lastChar == '\\' && c == '\\')
+        {
+          lastChar = ICharacterScanner.EOF;
+        }
+      }
+      i++;
+      lastChar = c;
+    }
+    return -1;
+  }
+
+  public void setRange(IDocument document, int offset, int length)
+  {
+    setPartialRange(document, offset, length, null, -1);
+  }
 
 
 }
