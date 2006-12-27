7a4bf122b3886fa07141c538581537a548a3da5a
jaysachs <jaysachs@6afd1a24-8710-0410-9bc4-c69fca332c4c>
2006-12-27 14:52:46 +0000

    Remove useless comment.
    git-svn-id: svn://svn.code.sf.net/p/jamon/code/trunk@2009 6afd1a24-8710-0410-9bc4-c69fca332c4c

--- eclipse/uiPlugin/src/org/jamon/eclipse/editor/JamonWordDetector.java
+++ eclipse/uiPlugin/src/org/jamon/eclipse/editor/JamonWordDetector.java
@@ -4,16 +4,10 @@ import org.eclipse.jface.text.rules.IWordDetector;
 
 public class JamonWordDetector implements IWordDetector {
 
-    /* (non-Javadoc)
-     * Method declared on IWordDetector.
-     */
     public boolean isWordPart(char character) {
         return Character.isJavaIdentifierPart(character);
     }
     
-    /* (non-Javadoc)
-     * Method declared on IWordDetector.
-     */
     public boolean isWordStart(char character) {
         return Character.isJavaIdentifierStart(character);
     }
