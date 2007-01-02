aa3d67732f6818e02600d9bcc820016bef582677
iroberts <iroberts@6afd1a24-8710-0410-9bc4-c69fca332c4c>
2007-01-02 05:29:05 +0000

    reformat according to project conventions.  No structural changes.
    git-svn-id: svn://svn.code.sf.net/p/jamon/code/trunk@2015 6afd1a24-8710-0410-9bc4-c69fca332c4c

--- eclipse/uiPlugin/src/org/jamon/eclipse/JamonProjectPlugin.java
+++ eclipse/uiPlugin/src/org/jamon/eclipse/JamonProjectPlugin.java
@@ -10,79 +10,101 @@ import org.eclipse.ui.plugin.AbstractUIPlugin;
 /**
  * The main plugin class to be used in the desktop.
  */
-public class JamonProjectPlugin extends AbstractUIPlugin {
-  //The shared instance.
-  private static JamonProjectPlugin plugin;
-  //Resource bundle.
-  private ResourceBundle resourceBundle;
+public class JamonProjectPlugin extends AbstractUIPlugin
+{
+    // The shared instance.
+    private static JamonProjectPlugin plugin;
 
+    // Resource bundle.
+    private ResourceBundle resourceBundle;
 
-  /**
-   * The constructor.
-   */
-  public JamonProjectPlugin() {
-    super();
-    plugin = this;
-    try {
-      resourceBundle = ResourceBundle.getBundle("org.jamon.JamonProjectPluginResources");
-    } catch (MissingResourceException x) {
-      resourceBundle = null;
-    }
-  }
-
-  /**
-   * Returns the shared instance.
-   */
-  public static JamonProjectPlugin getDefault() {
-    return plugin;
-  }
-
-    public static String getProxyMarkerType() {
-      return getDefault().pluginId() + ".proxyMarker";
+    /**
+     * The constructor.
+     */
+    public JamonProjectPlugin()
+    {
+        super();
+        plugin = this;
+        try
+        {
+            resourceBundle = ResourceBundle
+                    .getBundle("org.jamon.JamonProjectPluginResources");
+        }
+        catch (MissingResourceException x)
+        {
+            resourceBundle = null;
+        }
     }
 
-    public static String getImplMarkerType() {
-      return getDefault().pluginId() + ".implMarker";
+    /**
+     * Returns the shared instance.
+     */
+    public static JamonProjectPlugin getDefault()
+    {
+        return plugin;
     }
 
-    public static String getJamonMarkerType() {
-      return getDefault().pluginId() + ".jamonMarker";
+    public static String getProxyMarkerType()
+    {
+        return getDefault().pluginId() + ".proxyMarker";
     }
 
-    public static String getParentMarkerType() {
-      return getDefault().pluginId() + ".parentMarker";
+    public static String getImplMarkerType()
+    {
+        return getDefault().pluginId() + ".implMarker";
     }
 
-  public void logInfo(String p_message) {
-    getLog().log(new Status(IStatus.INFO, pluginId(), 0, p_message, null));
-  }
-
-  public void logError(Throwable p_error) {
-    String message = p_error.getMessage();
-    getLog().log(new Status(IStatus.ERROR, pluginId(), 0, message != null ? message : "", p_error));
-  }
-
-  /**
-   * Returns the string from the plugin's resource bundle,
-   * or 'key' if not found.
-   */
-  public static String getResourceString(String key) {
-    ResourceBundle bundle = JamonProjectPlugin.getDefault().getResourceBundle();
-    try {
-      return (bundle != null) ? bundle.getString(key) : key;
-    } catch (MissingResourceException e) {
-      return key;
+    public static String getJamonMarkerType()
+    {
+        return getDefault().pluginId() + ".jamonMarker";
     }
-  }
 
-  /**
-   * Returns the plugin's resource bundle,
-   */
-  public ResourceBundle getResourceBundle() {
-    return resourceBundle;
-  }
+    public static String getParentMarkerType()
+    {
+        return getDefault().pluginId() + ".parentMarker";
+    }
 
-  public String pluginId() {
-    return getBundle().getSymbolicName();
-  }
+    public void logInfo(String p_message)
+    {
+        getLog().log(new Status(IStatus.INFO, pluginId(), 0, p_message, null));
+    }
+
+    public void logError(Throwable p_error)
+    {
+        String message = p_error.getMessage();
+        getLog().log(new Status(IStatus.ERROR, pluginId(), 0, message != null
+            ? message
+            : "", p_error));
+    }
+
+    /**
+     * Returns the string from the plugin's resource bundle, or 'key' if not
+     * found.
+     */
+    public static String getResourceString(String key)
+    {
+        ResourceBundle bundle = JamonProjectPlugin.getDefault()
+                .getResourceBundle();
+        try
+        {
+            return (bundle != null) ? bundle.getString(key) : key;
+        }
+        catch (MissingResourceException e)
+        {
+            return key;
+        }
+    }
+
+    /**
+     * Returns the plugin's resource bundle,
+     */
+    public ResourceBundle getResourceBundle()
+    {
+        return resourceBundle;
+    }
+
+    public String pluginId()
+    {
+        return getBundle().getSymbolicName();
+    }
 }
