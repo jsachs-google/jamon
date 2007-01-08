6d5993e2b55837a4ded4b40714e83ae88e5fdd5a
iroberts <iroberts@6afd1a24-8710-0410-9bc4-c69fca332c4c>
2007-01-08 05:22:15 +0000

    Remove old style properties and code for loading them
    git-svn-id: svn://svn.code.sf.net/p/jamon/code/trunk@2020 6afd1a24-8710-0410-9bc4-c69fca332c4c

--- eclipse/uiPlugin/src/org/jamon/eclipse/editor/SimpleScanner.java
+++ eclipse/uiPlugin/src/org/jamon/eclipse/editor/SimpleScanner.java
@@ -1,9 +1,5 @@
 package org.jamon.eclipse.editor;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-
 import org.eclipse.swt.graphics.RGB;
 import org.eclipse.ui.PlatformUI;
 
@@ -36,49 +32,12 @@ public class SimpleScanner extends AbstractJavaContainingScanner
     private static RGB color(String propertyName, RGB defaultValue)
     {
         RGB themeRgb = themeColor(propertyName);
-        if (themeRgb != null) {
-            return themeRgb;
-        }
-        String s = COLORS.getProperty(propertyName);
-        if (s == null)
-        {
-            return defaultValue;
-        }
-        else
-        {
-            String[] rgb = s.trim().split(",");
-            return new RGB(Integer.valueOf(rgb[0].trim()), Integer.valueOf(rgb[1].trim()), Integer.valueOf(rgb[2].trim()));
-        }
+        return themeRgb != null ? themeRgb : defaultValue;
     }
 
     private static RGB themeColor(String propertyName) {
         return PlatformUI.getWorkbench().getThemeManager().getCurrentTheme().getColorRegistry().getRGB(
             THEME_BASE + propertyName);
-    }
-
-    private static final Properties COLORS = new Properties();
-    static
-    {
-        InputStream in = SimpleScanner.class.getResourceAsStream("colors.properties");
-        if (in != null)
-        {
-            try
-            {
-                COLORS.load(in);
-            }
-            catch (IOException e)
-            {
-                System.err.println("Unable to load colors");
-            }
-            try
-            {
-                in.close();
-            }
-            catch (IOException e)
-            {
-                // sshh
-            }
-        }
     }
 
     protected SimpleScanner(String p_openTag, String p_closeTag, String propertyBase)
