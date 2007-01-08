123f02eb789009c4cbc598a6ed7a832eb5c307c3
iroberts <iroberts@6afd1a24-8710-0410-9bc4-c69fca332c4c>
2007-01-08 02:08:37 +0000

    reformat to standards
    git-svn-id: svn://svn.code.sf.net/p/jamon/code/trunk@2018 6afd1a24-8710-0410-9bc4-c69fca332c4c

--- eclipse/uiPlugin/src/org/jamon/eclipse/editor/AbstractScanner.java
+++ eclipse/uiPlugin/src/org/jamon/eclipse/editor/AbstractScanner.java
@@ -7,55 +7,52 @@ import org.eclipse.jface.text.rules.ITokenScanner;
 
 public abstract class AbstractScanner implements ITokenScanner
 {
-  public int getTokenLength()
-  {
-    return tokenLength;
-  }
-  
-  public int getTokenOffset()
-  {
-    return tokenOffset;
-  }
-  
-  protected boolean lookingAt(int off, char[] match)
-  {
-    for (int i = 0; i < match.length; ++i)
+    public int getTokenLength()
     {
-      if (charAt(i + off) != match[i])
-      {
-        return false;
-      }
+        return tokenLength;
     }
-    return true;
-  }
 
-  protected int charAt(int i)
-  {
-    try
+    public int getTokenOffset()
     {
-      return document.getChar(i);
+        return tokenOffset;
     }
-    catch (BadLocationException e)
+
+    protected boolean lookingAt(int off, char[] match)
     {
-      return ICharacterScanner.EOF;
+        for (int i = 0; i < match.length; ++i)
+        {
+            if (charAt(i + off) != match[i]) { return false; }
+        }
+        return true;
     }
-  }
 
-  public void setRange(IDocument p_document, int p_offset, int p_length)
-  {
-    this.document = p_document;
-    this.initialOffset = p_offset;
-    this.offset = p_offset;
-    this.length = p_length;
-    this.limit = offset + length;
-  }
+    protected int charAt(int i)
+    {
+        try
+        {
+            return document.getChar(i);
+        }
+        catch (BadLocationException e)
+        {
+            return ICharacterScanner.EOF;
+        }
+    }
 
-  protected IDocument document;
-  protected int offset;
-  protected int initialOffset;
-  protected int length;
-  protected int tokenOffset;
-  protected int tokenLength;
-  protected int limit;
+    public void setRange(IDocument p_document, int p_offset, int p_length)
+    {
+        this.document = p_document;
+        this.initialOffset = p_offset;
+        this.offset = p_offset;
+        this.length = p_length;
+        this.limit = offset + length;
+    }
+
+    protected IDocument document;
+    protected int offset;
+    protected int initialOffset;
+    protected int length;
+    protected int tokenOffset;
+    protected int tokenLength;
+    protected int limit;
 
 }
