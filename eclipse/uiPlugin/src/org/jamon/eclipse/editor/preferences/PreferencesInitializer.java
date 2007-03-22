b63bbb9afc5f2de70d752518e478d8f8ee410d27
iroberts <iroberts@6afd1a24-8710-0410-9bc4-c69fca332c4c>
2007-03-22 05:28:24 +0000

    Provide some half decent defaults.
    Well, quarter decent, anyway...
    
    git-svn-id: svn://svn.code.sf.net/p/jamon/code/trunk@2115 6afd1a24-8710-0410-9bc4-c69fca332c4c

--- eclipse/uiPlugin/src/org/jamon/eclipse/editor/preferences/PreferencesInitializer.java
+++ eclipse/uiPlugin/src/org/jamon/eclipse/editor/preferences/PreferencesInitializer.java
@@ -6,16 +6,11 @@ import java.io.InputStream;
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.preference.PreferenceStore;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.graphics.RGB;
 import org.jamon.eclipse.EclipseUtils;
 import org.jamon.eclipse.JamonProjectPlugin;
 
 public class PreferencesInitializer extends AbstractPreferenceInitializer
 {
-    private static final String BLACK = SyntaxPreferences.rgbToString(new RGB(0,0,0));
-    private static final String DEFAULT = "";
-    
     @Override public void initializeDefaultPreferences()
     {
         IPreferenceStore preferenceStore = JamonProjectPlugin.getDefault().getPreferenceStore();
@@ -27,19 +22,15 @@ public class PreferencesInitializer extends AbstractPreferenceInitializer
     {
         for (SyntaxType syntaxType : SyntaxType.values())
         {
-            preferenceStore.setDefault(syntaxType.getStylePreferenceKey(), SWT.NONE);
-            preferenceStore.setDefault(syntaxType.getBackgroundPreferenceKey(), DEFAULT);
-            preferenceStore.setDefault(syntaxType.getForegroundPreferenceKey(), BLACK);
+            preferenceStore.setDefault(
+                syntaxType.getStylePreferenceKey(), syntaxType.getDefaultSwtStyle());
+            preferenceStore.setDefault(
+                syntaxType.getForegroundPreferenceKey(),
+                SyntaxPreferences.rgbToString(syntaxType.getDefaultForeground()));
+            preferenceStore.setDefault(
+                syntaxType.getBackgroundPreferenceKey(),
+                SyntaxPreferences.rgbToString(syntaxType.getDefaultBackground()));
         }
-
-        preferenceStore.setDefault(SyntaxType.DOC_TAG.getStylePreferenceKey(), SWT.BOLD);
-        preferenceStore.setDefault(
-            SyntaxType.DOC_TAG.getForegroundPreferenceKey(),
-            SyntaxPreferences.rgbToString(new RGB(127, 127, 127)));
-        preferenceStore.setDefault(SyntaxType.DOC_BODY.getStylePreferenceKey(), SWT.ITALIC);
-        preferenceStore.setDefault(
-            SyntaxType.DOC_BODY.getForegroundPreferenceKey(),
-            SyntaxPreferences.rgbToString(new RGB(112, 112, 112)));
     }
 
     private void loadFromStandardPreferences(IPreferenceStore preferenceStore)
--- eclipse/uiPlugin/src/org/jamon/eclipse/editor/preferences/SyntaxType.java
+++ eclipse/uiPlugin/src/org/jamon/eclipse/editor/preferences/SyntaxType.java
@@ -3,25 +3,32 @@ package org.jamon.eclipse.editor.preferences;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.RGB;
+
     public enum SyntaxType {
-        ARGS("Args", "args"),
-        XARGS("Xargs", "xargs"),
-        JAVA("Java code", "java"),
-        ALIAS("Alias", "alias"),
-        CLASS("Class", "class"),
-        IMPORT("Imports", "imports"),
-        CALL("Call", "call"),
-        CALL_PATH("Call path", "callPath"),
-        FLOW_CONTROL("Flow control", "flowControl"),
-        DEF("Def", "def"),
-        METHOD("Method", "method"),
-        EMIT("Emit", "emit"),
-        DOC_TAG("Doc Tag", "docTag"),
-        DOC_BODY("Doc Body", "docBody");
+        ARGS("Args", "args", SWT.NONE, gray(0), gray(229)),
+        XARGS("Xargs", "xargs", SWT.NONE, gray(0), gray(255)),
+        JAVA("Java code", "java", SWT.BOLD, gray(0), gray(240)),
+        ALIAS("Alias", "alias", SWT.NONE, gray(0), gray(255)),
+        CLASS("Class", "class", SWT.NONE, gray(0), gray(255)),
+        IMPORT("Imports", "imports", SWT.NONE, gray(0), gray(191)),
+        CALL("Call", "call", SWT.BOLD, gray(0), gray(255)),
+        CALL_PATH("Call path", "callPath", SWT.ITALIC, gray(0), gray(255)),
+        FLOW_CONTROL("Flow control", "flowControl", SWT.NONE, gray(0), new RGB(255, 255, 0)),
+        DEF("Def", "def", SWT.NONE, gray(0), new RGB(230, 230, 250)),
+        METHOD("Method", "method", SWT.NONE, gray(0), gray(255)),
+        EMIT("Emit", "emit", SWT.BOLD, gray(0), new RGB(191, 242, 191)),
+        DOC_TAG("Doc Tag", "docTag", SWT.BOLD, gray(127), gray(255)),
+        DOC_BODY("Doc Body", "docBody", SWT.ITALIC, gray(112), gray(255));
 
-    private SyntaxType(String p_label, String p_preferenceKey)
+    private SyntaxType(
+        String p_label,
+        String p_preferenceKey,
+        int p_defaultSwtStyle,
+        RGB p_defaultForeground,
+        RGB p_defaultBackground)
     {
-
         m_label = p_label;
         m_stylePreferenceKey =
             SyntaxPreferences.SYNTAX_PREF_BASE + p_preferenceKey + SyntaxPreferences.STYLE_PREF_KEY;
@@ -29,13 +36,23 @@ import java.util.Map;
             SyntaxPreferences.SYNTAX_PREF_BASE + p_preferenceKey + SyntaxPreferences.FOREGROUND_PREF_KEY;
         m_backgroundPreferenceKey =
             SyntaxPreferences.SYNTAX_PREF_BASE + p_preferenceKey + SyntaxPreferences.BACKGROUND_PREF_KEY;
+        m_defaultSwtStyle = p_defaultSwtStyle;
+        m_defaultForeground = p_defaultForeground;
+        m_defaultBackground = p_defaultBackground;
+    }
 
+    private static RGB gray(int p_brightness)
+    {
+        return new RGB(p_brightness, p_brightness, p_brightness);
     }
 
     public String getLabel() { return m_label; }
     public String getStylePreferenceKey() { return m_stylePreferenceKey; }
     public String getForegroundPreferenceKey() { return m_foregroundPreferenceKey; }
     public String getBackgroundPreferenceKey() { return m_backgroundPreferenceKey; }
+    public int getDefaultSwtStyle() { return m_defaultSwtStyle; }
+    public RGB getDefaultForeground() { return m_defaultForeground; }
+    public RGB getDefaultBackground() { return m_defaultBackground; }
 
     public static SyntaxType getByPreferenceKey(String p_preferencekey)
     {
@@ -46,6 +63,8 @@ import java.util.Map;
     private final String m_stylePreferenceKey;
     private final String m_foregroundPreferenceKey;
     private final String m_backgroundPreferenceKey;
+    private final int m_defaultSwtStyle;
+    private final RGB m_defaultForeground, m_defaultBackground;
 
     private final static Map<String, SyntaxType> s_typesByPreferenceKey =
         new HashMap<String, SyntaxType>();
