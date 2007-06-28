2ca28a2ca41170b99a308faa0d0a5bc587dc033a
jaysachs <jaysachs@6afd1a24-8710-0410-9bc4-c69fca332c4c>
2007-06-28 12:41:59 +0000

    Incremental mavenization of doc project
    git-svn-id: svn://svn.code.sf.net/p/jamon/code/trunk@2336 6afd1a24-8710-0410-9bc4-c69fca332c4c

--- doc/sample/servlet/src/HelloServlet.java
+++ doc/sample/servlet/src/HelloServlet.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.jamon.RecompilingTemplateManager;
+import org.jamon.compiler.RecompilingTemplateManager;
 import org.jamon.TemplateManagerSource;
 
 public final class HelloServlet
